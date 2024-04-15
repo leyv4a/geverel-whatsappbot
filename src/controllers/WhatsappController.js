a3bb07acbdb342b74ceaeec5e46ee9831e0e2fde
leyv4a <gabriel.leyva228902@potros.itson.edu.mx>
2024-04-15 12:06:04 -0700

    a

--- src/controllers/WhatsappController.js
+++ src/controllers/WhatsappController.js
@@ -1,3 +1,6 @@
+import fs from 'fs';
+const myConsole = new console.Console(fs.createWriteStream("./logs.txt"));
+
 export const verifyToken = (req,res) => {
     try {   
         var accessToken = 'GSAFJSAOFJSANOAHNSAKFNSAOKFJSAOK';
@@ -5,20 +8,29 @@ export const verifyToken = (req,res) => {
         var challenge = req.query['hub.challenge'];
         
         if(challenge != null && token != null && token === accessToken) {
-            res.send(challenge)
+            res.status(200).send(challenge)
         }else{
             res.status(400).send('Error, wrong token')
         }
     } catch (e) {
         res.status(400).send(e);
     }
-
-
-
 }
 
 export const receivedMessage = (req,res) => {
-res.send('Received message')
 
+    try {
+        var entry = (req.body["entry"])[0];
+        var changes = (entry['changes'][0]);
+        var value = changes["value"];
+        var messageObject = value['messages'];
+ 
+
+        myConsole.log(messageObject)
+        res.send('EVENT_RECEIVED');
+    } catch (e) {
+        myConsole.log(e)
+        res.send('EVENT_RECEIVED');
+    }
 
 }
